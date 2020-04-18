e79abe2ffac04ddc828d73466ad17e3092dfdb24
Kevin Frans <kevinfrans2@gmail.com>
2020-04-18 01:41:47 -0700

    base graphics

---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour {
+
+    public Transform wall;
+    public Transform floor;
+
+    // Update is called once per frame
+    void Start() {
+        for (int x = 0; x < 20; x++) {
+            for (int y = 0; y < 20; y++) {
+                Transform atTile = null;
+                if ((x == 0 || x == 19 || y == 0 || y == 19) && x % 3 == 0) {
+                    atTile = wall;
+                }
+                else {
+                    atTile = floor;
+                }
+
+                Transform t = Instantiate(atTile, new Vector3(x*2, y*2, 0), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    private Rigidbody2D rb;
+    public Transform spriteBody;
+    
+    private float bodyrot;
+    
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        if (Mathf.Abs(horiz) < 0.1f) {
+            horiz = 0;
+        }
+        if (Mathf.Abs(vert) < 0.1f) {
+            vert = 0;
+        }
+        rb.velocity = new Vector2(horiz, vert) * 5;
+
+        Vector3 mousePosition = Input.mousePosition;
+        float ang = Mathf.Atan2( mousePosition.y - Screen.height/2.0f,  mousePosition.x - Screen.width/2.0f) * Mathf.Rad2Deg + 90;
+        
+        if (rb.velocity.magnitude > 0) {
+            bodyrot += Time.deltaTime * 10;
+        }
+        spriteBody.localEulerAngles = new Vector3(0, 0, ang + Mathf.Sin(bodyrot)*30);
+    }
+    
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z) {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+}
