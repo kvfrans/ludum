ccc021fbf568807f6abbda5b3be776b9a5646b0b
jma22 <chunming2000@gmail.com>
2020-04-18 18:15:07 -0500

    lol

--- Assets/Scripts/buttonController.cs
+++ Assets/Scripts/buttonController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class buttonController : MonoBehaviour
+public class ButtonController : MonoBehaviour
 {
     // Start is called before the first frame update
     private SpriteRenderer SR;
