ee73ba51cba944a05a27af9776b359c7f0762b7e
jma22 <chunming2000@gmail.com>
2020-04-18 20:00:23 -0500

    lmao

--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class NoteController : MonoBehaviour
 {
     public Vector3 speed;
+    public string zone;
+    public KeyCode pressKey;
     // Start is called before the first frame update
     void Start()
     {
-        
+        zone = "None";
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.localPosition = transform.localPosition + speed * Time.deltaTime;
+        if (zone =="clicker"){
+            if (Input.GetKeyDown(pressKey)){
+                gameObject.SetActive(false);
+            }
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag== "clicker"){
+            zone = "clicker";
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag== "clicker"){
+            zone = "None";
+        }
     }
 }
+
--- Assets/Scripts/buttonController.cs
+++ Assets/Scripts/buttonController.cs
@@ -27,4 +27,5 @@ public class ButtonController : MonoBehaviour
             SR.sprite = unclickImg;
         }
     }
+    
 }
